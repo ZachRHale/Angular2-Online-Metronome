f4b9855b312373fb343a865ea972cafacf51f39d
Zachary Hale <zachary.hale88@gmail.com>
2017-06-10 18:28:05 -0400

    Added the ASP.NET CORE MVC backend to this project as well

---
+++ OnlineMetronomeREST/Controllers/api/MeasureController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineMetronomeREST.Models;
+
+namespace OnlineMetronomeREST.Controllers.api
+{
+    
+    public class MeasuresController : Controller
+    {
+        
+        public JsonResult Index()
+        {
+            var someMeasures = new List<Measure>();
+            someMeasures.Add(new Measure(5, 4, 80));
+            someMeasures.Add(new Measure(10, 8, 90));
+
+            return Json(someMeasures);
+        }
+
+        [HttpGet]
+        public JsonResult Zachhale(int id) 
+        {
+            var someMeasures = new List<Measure>(); 
+
+            if (id == 0){
+				
+				someMeasures.Add(new Measure(1, 4, 80));
+				someMeasures.Add(new Measure(2, 4, 80));
+
+				return Json(someMeasures);  
+            } else if (id == 1){
+				someMeasures.Add(new Measure(6, 4, 80));
+				someMeasures.Add(new Measure(9, 8, 90));
+                someMeasures.Add(new Measure(2, 4, 70));
+
+				return Json(someMeasures);
+            } else {
+				someMeasures.Add(new Measure(1, 4, 80));
+				someMeasures.Add(new Measure(1, 8, 90));
+				someMeasures.Add(new Measure(1, 2, 70));
+
+				return Json(someMeasures);
+            }
+
+			
+        }
+    }
+}
---
+++ OnlineMetronomeREST/Models/Measure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace OnlineMetronomeREST.Models
+{
+    public class Measure
+    {
+        public int Top { get; private set; }
+        public int Bottom { get; private set; }
+        public int Tempo { get; private set; }
+
+        public Measure(int top, int bottom, int tempo) 
+        {
+            Top = top;
+            Bottom = bottom;
+            Tempo = tempo;
+
+        }
+    }
+}
