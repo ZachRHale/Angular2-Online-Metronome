2ef5d4f29123ff9193aac78e79b43b0624e8bc0f
Zachary Hale <zachary.hale88@gmail.com>
2017-06-17 04:32:27 -0400

    Moved component template into a separate html file

--- OnlineMetronomeREST/src/app/components/embedsortable.component.ts
+++ OnlineMetronomeREST/src/app/components/embedsortable.component.ts
@@ -5,50 +5,8 @@ import { MeasureService } from '../services/measures.service';
 
 @Component({
     selector: 'embedded-sortable',
-    template: `
-    <h1>Angular 4 Javascript Metronome</h1>
-    <div class="row">        
-        <button class="btn btn-default" (click)="playMeasureList(0)">Play All Measures</button>
-        <button class="btn btn-default" (click)="printStuff()">Print the list</button>
-      </div>
-      <div class="row">
-      <div class="col-md-4">
-      <div class="form-group add-measures">
-        <input class="form-control input-lg" type="number" #topNumber />
-        <input class="form-control input-lg" type="number" #bottomNumber />
-        <button class="btn btn-default btn-lg" (click)="addMeasure(topNumber.value, bottomNumber.value)">Add Measure</button>
-      </div>
-      <div class="panel panel-warning">
-        <div class="panel-heading">
-            <h3>Available measures</h3>
-        </div>
-        <div class="panel-body" dnd-sortable-container [sortableData]="listMeasures" [dropZones]="['boxers-zone']">
-          <ul class="list-group" >
-            <li *ngFor="let item of listMeasures; let i = index" class="list-group-item" [dragEnabled]="true" [dragData]="item" dnd-sortable [sortableIndex]="i">{{item.top}} / {{item.bottom}}</li>           
-          </ul>
-        </div>
-      </div>
-      </div>
-      <div class="col-md-8">
-      <div class="form-group load-measures">
-        <button class="btn btn-default btn-lg" (click)="loadMeasures(HTTPAddress.value)">Load Measures</button>
-        <input class="form-control input-lg" #HTTPAddress />
-      </div>
-        <div class="panel panel-info">
-          <div class="panel-heading">
-            <h3>The Piece</h3>
-          </div>
-          <div class="panel-body" dnd-droppable (onDropSuccess)="add($event)" [dropZones]="['boxers-zone']" >
-          <div dnd-sortable-container [sortableData]="measures">
-            <div class="list-group">
-              <div *ngFor="let item of measures; let i = index" dnd-sortable [sortableIndex]="i" class="list-group-item">{{item.top}} / {{item.bottom}}</div>
-            </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-`
+    moduleId: module.id,
+    templateUrl: './embedsortable.html'
 })
 
 
